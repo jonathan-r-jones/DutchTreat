a529db1c4b9557a6fed32d3e26c1ce741ed046a1
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-09-12 18:14:54 -0400

    Sep-12-2018_6:14_PM

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -15,6 +15,7 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,9 +30,14 @@ namespace DutchTreat
             //{
             //    await context.Response.WriteAsync("Hello World from DutchTreat!");
             //});
-            app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseNodeModules(env);
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                "{controller}/{action}/{id?}",
+                new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,16 +1,30 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
 
-console.log("Hello Pluralsight.");
+    var x = 0;
+    var s = "";
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    console.log("Hello Pluralsight.");
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function (){
-    console.log("Buying Item");
-});
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var productInfo = document.getElementsByClassName("product-props");
-    
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
 
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+
+
+
+})
